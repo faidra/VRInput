2c9583f78e0e5c6a943a97f6e621f3a6c8704b1c
faidra <faidra.windmark@gmail.com>
2019-05-22 01:04:09 +0900

    KeyMasterStoreを作成

---
+++ Assets/VRInput/KeyMasterStore.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class KeyMasterStore : MonoBehaviour
+{
+    [SerializeField]
+    KeyMasterEditor Editor;
+
+    public KeyMaster KeyMaster { get; private set; }
+
+    void Start()
+    {
+        Editor.Set(KeyMasterIO.Read());
+        Editor.OnValueChangedAsObservable()
+            .Subscribe(OnChanged)
+            .AddTo(this);
+    }
+
+    void OnChanged(KeyMaster value)
+    {
+        KeyMaster = value;
+        KeyMasterIO.Write(value);
+    }
+}
