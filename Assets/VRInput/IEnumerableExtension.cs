4ed83ae1201902c61559fb560252d98df4ab77ff
faidra <faidra.windmark@gmail.com>
2019-05-21 22:10:14 +0900

    make WithIndex

---
+++ Assets/VRInput/IEnumerableExtension.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UniRx;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class IEnumerableExtension
+{
+    public static IEnumerable<ValueTuple<T, int>> WithIndex<T>(this IEnumerable<T> source) => source.Select(ValueTuple.Create<T, int>);
+}
