eda16204efc8ca483eda0738d275371a6efd6712
faidra <faidra.windmark@gmail.com>
2019-05-21 22:13:11 +0900

    make togglebuttons

---
+++ Assets/VRInput/ToggleButtons.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using UniRx;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ToggleButtons : MonoBehaviour
+{
+    [Serializable]
+    struct ButtonAndGameObject
+    {
+        public Button Button;
+        public GameObject GameObject;
+    }
+    [SerializeField]
+    ButtonAndGameObject[] ButtonAndGameObjects;
+
+    void Start()
+    {
+        ButtonAndGameObjects
+            .Select((bg, i) => bg.Button.OnClickAsObservable().Select(_ => i))
+            .Merge()
+            .StartWith(0)
+            .Subscribe(i =>
+        {
+            foreach (var bg in ButtonAndGameObjects.WithIndex())
+            {
+                bg.Item1.Button.interactable = bg.Item2 != i;
+                bg.Item1.GameObject.SetActive(bg.Item2 == i);
+            }
+        })
+        .AddTo(this);
+    }
+}
