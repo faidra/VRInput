14722e8ecfb8db47d379ad1a4227ef94c951ed2e
faidra <faidra.windmark@gmail.com>
2019-05-23 14:08:52 +0900

    AdvancedSettingsを作成

---
+++ Assets/VRInput/AdvancedSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using WindowsInput.Native;
+
+public class AdvancedSettings
+{
+    public KeyMapping[] KeyMappings;
+    public KeyReplacing KeyReplacing;
+}
+
+public class AdvancedSettingsInfo
+{
+    public string[] KeyMappingPaths { get; set; }
+    public string KeyReplacingPath { get; set; }
+}
+
+public class KeyMapping
+{
+    public Dictionary<string, VirtualKeyCode[]> Values { get; set; }
+
+    public IEnumerable<VirtualKeyCode> Map(string str)
+    {
+        if (Values.TryGetValue(str, out var v)) return v;
+        return null;
+    }
+}
+
+public class KeyReplacing
+{
+    public static KeyReplacing Empty = new KeyReplacing { Values = new Dictionary<VirtualKeyCode, VirtualKeyCode>() };
+
+    public Dictionary<VirtualKeyCode, VirtualKeyCode> Values { get; set; }
+
+    public VirtualKeyCode Replace(VirtualKeyCode from)
+    {
+        if (Values.TryGetValue(from, out var to)) return to;
+        return from;
+    }
+}
---
+++ Assets/VRInput/AdvancedSettingsStore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using YamlDotNet.Serialization;
+using System.IO;
+using System.Linq;
+
+public class AdvancedSettingsStore : MonoBehaviour
+{
+    const string AdvancedSettingsInfoPath = "AdvancedSettings.yml";
+
+    AdvancedSettings advancedSettings;
+    public AdvancedSettings AdvancedSettings
+    {
+        get
+        {
+            if (advancedSettings == null) advancedSettings = GenerateSettings();
+            return advancedSettings;
+        }
+    }
+
+    AdvancedSettings GenerateSettings()
+    {
+        var info = DeserializeFromFile<AdvancedSettingsInfo>(AdvancedSettingsInfoPath);
+        var mappings = info.KeyMappingPaths.Select(DeserializeFromFile<KeyMapping>).ToArray();
+        var replacing = string.IsNullOrEmpty(info.KeyReplacingPath) ?
+            KeyReplacing.Empty :
+            DeserializeFromFile<KeyReplacing>(info.KeyReplacingPath);
+        return new AdvancedSettings { KeyMappings = mappings, KeyReplacing = replacing };
+    }
+
+    T DeserializeFromFile<T>(string path)
+    {
+        var deserializer = new Deserializer();
+        using (var reader = new StreamReader(path))
+            return deserializer.Deserialize<T>(reader);
+    }
+}
